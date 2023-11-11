6e2409eae9b7e7fbc2c1b3b9ccd6db23663ca4d3
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-11-11 18:12:31 +0200

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+void Func(FILE* File, int key)
+{
+    FILE* Cipher = fopen("cipher.txt", "w");
+    char c;
+
+    while ((c = fgetc(File)) != EOF)
+    {
+        if (c >= 'A' && c <= 'Z')
+        {
+            c = (c - 'A' + key) % 26 + 'A';
+        }
+        else if (c >= 'a' && c <= 'z')
+        {
+            c = (c - 'a' + key) % 26 + 'a';
+        }
+        fputc(c, Cipher);
+    }
+        fclose(Cipher);
+}
+       
+
+int main()
+{
+    setlocale(0, "RU");
+    int key = 3;
+
+    FILE* MyFile = fopen("text.txt", "w");
+    fprintf(MyFile, "Eat some more of these soft French rolls and drink some tea");
+    fclose(MyFile);
+    MyFile = fopen("text.txt", "r");
+    
+    Func(MyFile, key);
+    fclose(MyFile);
+}
